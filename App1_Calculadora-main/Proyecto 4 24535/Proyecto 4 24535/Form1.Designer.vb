8d19c4af4f1d853028d5d253690cc67d8a0401c3
diego-martinez <diego.martinez04@iest.edu.mx>
2026-02-13 08:31:34 -0600

    Agregamos teclado numérico

---
+++ App1_Calculadora-main/Proyecto 4 24535/Proyecto 4 24535/Form1.Designer.vb
@@ -0,0 +1,310 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.botonmas = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.botonmenos = New System.Windows.Forms.Button()
+        Me.botonmultiplicacion = New System.Windows.Forms.Button()
+        Me.botondivision = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Boton2 = New System.Windows.Forms.Button()
+        Me.Boton3 = New System.Windows.Forms.Button()
+        Me.Boton4 = New System.Windows.Forms.Button()
+        Me.Boton5 = New System.Windows.Forms.Button()
+        Me.Boton6 = New System.Windows.Forms.Button()
+        Me.botoon6 = New System.Windows.Forms.Button()
+        Me.Boton8 = New System.Windows.Forms.Button()
+        Me.Boton9 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Variable Text Semiligh", 25.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(45, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(190, 46)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "NUMERO 1"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(302, 44)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(228, 45)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "0"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI Variable Text Semiligh", 25.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(45, 121)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(195, 46)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "NUMERO 2"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(302, 123)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(228, 45)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.Text = "0"
+        '
+        'botonmas
+        '
+        Me.botonmas.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.botonmas.Location = New System.Drawing.Point(753, 44)
+        Me.botonmas.Name = "botonmas"
+        Me.botonmas.Size = New System.Drawing.Size(87, 89)
+        Me.botonmas.TabIndex = 4
+        Me.botonmas.Text = "+"
+        Me.botonmas.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI Variable Text Semiligh", 25.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(45, 214)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(207, 46)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "RESULTADO"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Cambria Math", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(309, 238)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 195)
+        Me.Label4.TabIndex = 6
+        '
+        'botonmenos
+        '
+        Me.botonmenos.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.botonmenos.Location = New System.Drawing.Point(753, 144)
+        Me.botonmenos.Name = "botonmenos"
+        Me.botonmenos.Size = New System.Drawing.Size(87, 89)
+        Me.botonmenos.TabIndex = 7
+        Me.botonmenos.Text = "-"
+        Me.botonmenos.UseVisualStyleBackColor = True
+        '
+        'botonmultiplicacion
+        '
+        Me.botonmultiplicacion.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.botonmultiplicacion.Location = New System.Drawing.Point(753, 239)
+        Me.botonmultiplicacion.Name = "botonmultiplicacion"
+        Me.botonmultiplicacion.Size = New System.Drawing.Size(87, 89)
+        Me.botonmultiplicacion.TabIndex = 8
+        Me.botonmultiplicacion.Text = "x"
+        Me.botonmultiplicacion.UseVisualStyleBackColor = True
+        '
+        'botondivision
+        '
+        Me.botondivision.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.botondivision.Location = New System.Drawing.Point(753, 344)
+        Me.botondivision.Name = "botondivision"
+        Me.botondivision.Size = New System.Drawing.Size(87, 89)
+        Me.botondivision.TabIndex = 9
+        Me.botondivision.Text = "÷"
+        Me.botondivision.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!)
+        Me.Button1.Location = New System.Drawing.Point(361, 421)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(87, 87)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!)
+        Me.Button2.Location = New System.Drawing.Point(460, 518)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(87, 87)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "0"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Boton2
+        '
+        Me.Boton2.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!)
+        Me.Boton2.Location = New System.Drawing.Point(460, 421)
+        Me.Boton2.Name = "Boton2"
+        Me.Boton2.Size = New System.Drawing.Size(87, 87)
+        Me.Boton2.TabIndex = 12
+        Me.Boton2.Text = "2"
+        Me.Boton2.UseVisualStyleBackColor = True
+        '
+        'Boton3
+        '
+        Me.Boton3.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!)
+        Me.Boton3.Location = New System.Drawing.Point(553, 421)
+        Me.Boton3.Name = "Boton3"
+        Me.Boton3.Size = New System.Drawing.Size(87, 87)
+        Me.Boton3.TabIndex = 13
+        Me.Boton3.Text = "3"
+        Me.Boton3.UseVisualStyleBackColor = True
+        '
+        'Boton4
+        '
+        Me.Boton4.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!)
+        Me.Boton4.Location = New System.Drawing.Point(361, 328)
+        Me.Boton4.Name = "Boton4"
+        Me.Boton4.Size = New System.Drawing.Size(87, 87)
+        Me.Boton4.TabIndex = 14
+        Me.Boton4.Text = "4"
+        Me.Boton4.UseVisualStyleBackColor = True
+        '
+        'Boton5
+        '
+        Me.Boton5.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!)
+        Me.Boton5.Location = New System.Drawing.Point(460, 328)
+        Me.Boton5.Name = "Boton5"
+        Me.Boton5.Size = New System.Drawing.Size(87, 87)
+        Me.Boton5.TabIndex = 15
+        Me.Boton5.Text = "5"
+        Me.Boton5.UseVisualStyleBackColor = True
+        '
+        'Boton6
+        '
+        Me.Boton6.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!)
+        Me.Boton6.Location = New System.Drawing.Point(553, 328)
+        Me.Boton6.Name = "Boton6"
+        Me.Boton6.Size = New System.Drawing.Size(87, 87)
+        Me.Boton6.TabIndex = 16
+        Me.Boton6.Text = "6"
+        Me.Boton6.UseVisualStyleBackColor = True
+        '
+        'botoon6
+        '
+        Me.botoon6.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!)
+        Me.botoon6.Location = New System.Drawing.Point(361, 235)
+        Me.botoon6.Name = "botoon6"
+        Me.botoon6.Size = New System.Drawing.Size(87, 87)
+        Me.botoon6.TabIndex = 17
+        Me.botoon6.Text = "7"
+        Me.botoon6.UseVisualStyleBackColor = True
+        '
+        'Boton8
+        '
+        Me.Boton8.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!)
+        Me.Boton8.Location = New System.Drawing.Point(460, 235)
+        Me.Boton8.Name = "Boton8"
+        Me.Boton8.Size = New System.Drawing.Size(87, 87)
+        Me.Boton8.TabIndex = 18
+        Me.Boton8.Text = "8"
+        Me.Boton8.UseVisualStyleBackColor = True
+        '
+        'Boton9
+        '
+        Me.Boton9.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!)
+        Me.Boton9.Location = New System.Drawing.Point(553, 235)
+        Me.Boton9.Name = "Boton9"
+        Me.Boton9.Size = New System.Drawing.Size(87, 87)
+        Me.Boton9.TabIndex = 19
+        Me.Boton9.Text = "9"
+        Me.Boton9.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold)
+        Me.Button3.Location = New System.Drawing.Point(553, 518)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(87, 89)
+        Me.Button3.TabIndex = 20
+        Me.Button3.Text = "CE"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightCyan
+        Me.ClientSize = New System.Drawing.Size(875, 617)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Boton9)
+        Me.Controls.Add(Me.Boton8)
+        Me.Controls.Add(Me.botoon6)
+        Me.Controls.Add(Me.Boton6)
+        Me.Controls.Add(Me.Boton5)
+        Me.Controls.Add(Me.Boton4)
+        Me.Controls.Add(Me.Boton3)
+        Me.Controls.Add(Me.Boton2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.botondivision)
+        Me.Controls.Add(Me.botonmultiplicacion)
+        Me.Controls.Add(Me.botonmenos)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.botonmas)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents botonmas As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents botonmenos As Button
+    Friend WithEvents botonmultiplicacion As Button
+    Friend WithEvents botondivision As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Boton2 As Button
+    Friend WithEvents Boton3 As Button
+    Friend WithEvents Boton4 As Button
+    Friend WithEvents Boton5 As Button
+    Friend WithEvents Boton6 As Button
+    Friend WithEvents botoon6 As Button
+    Friend WithEvents Boton8 As Button
+    Friend WithEvents Boton9 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ App1_Calculadora-main/Proyecto 4 24535/Proyecto 4 24535/Form1.vb
@@ -0,0 +1,93 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles botonmas.Click
+        Dim Numero1 As Integer = Integer.Parse(TextBox1.Text)
+        Dim Numero2 As Integer = Integer.Parse(TextBox2.Text)
+
+        Dim resultado As Integer = Numero1 + Numero2
+
+        Label4.Text = resultado.ToString()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles botonmenos.Click
+        Dim Numero1 As Integer = Integer.Parse(TextBox1.Text)
+        Dim Numero2 As Integer = Integer.Parse(TextBox2.Text)
+
+        Dim resultado As Integer = Numero1 - Numero2
+
+        Label4.Text = resultado.ToString()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles botonmultiplicacion.Click
+        Dim Numero1 As Integer = Integer.Parse(TextBox1.Text)
+        Dim Numero2 As Integer = Integer.Parse(TextBox2.Text)
+
+        Dim resultado As Integer = Numero1 * Numero2
+
+        Label4.Text = resultado.ToString()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles botondivision.Click
+        Dim Numero1 As Integer = Integer.Parse(TextBox1.Text)
+        Dim Numero2 As Integer = Integer.Parse(TextBox2.Text)
+
+        Dim resultado As Integer = Numero1 / Numero2
+
+        Label4.Text = resultado.ToString()
+    End Sub
+
+    Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Text = TextBox1.Text & "0"
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text = TextBox1.Text & "1"
+    End Sub
+
+    Private Sub Boton2_Click(sender As Object, e As EventArgs) Handles Boton2.Click
+        TextBox1.Text = TextBox1.Text & "2"
+    End Sub
+
+    Private Sub Boton3_Click(sender As Object, e As EventArgs) Handles Boton3.Click
+        TextBox1.Text = TextBox1.Text & "3"
+    End Sub
+
+    Private Sub Boton4_Click(sender As Object, e As EventArgs) Handles Boton4.Click
+        TextBox1.Text = TextBox1.Text & "4"
+    End Sub
+
+    Private Sub Boton5_Click(sender As Object, e As EventArgs) Handles Boton5.Click
+        TextBox1.Text = TextBox1.Text & "5"
+    End Sub
+
+    Private Sub Boton6_Click(sender As Object, e As EventArgs) Handles Boton6.Click
+        TextBox1.Text = TextBox1.Text & "6"
+    End Sub
+
+    Private Sub botoon6_Click(sender As Object, e As EventArgs) Handles botoon6.Click
+        TextBox1.Text = TextBox1.Text & "7"
+    End Sub
+
+    Private Sub Boton8_Click(sender As Object, e As EventArgs) Handles Boton8.Click
+        TextBox1.Text = TextBox1.Text & "8"
+    End Sub
+
+    Private Sub Boton9_Click(sender As Object, e As EventArgs) Handles Boton9.Click
+        TextBox1.Text = TextBox1.Text & "9"
+    End Sub
+
+    Private Sub Button3_Click_1(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBox1.Text = ""
+    End Sub
+End Class
